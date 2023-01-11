88d7f87e50e2d543881b3c55dbe097784c1db8ff
guadalupe <guadalupe.alavila@gmail.com>
2023-01-11 01:54:24 -0300

    add Main screen

--- TodoFirstVersion/app/src/main/java/com/guadalavila/todofirstversion/MainActivity.kt
+++ TodoFirstVersion/app/src/main/java/com/guadalavila/todofirstversion/MainActivity.kt
@@ -2,10 +2,24 @@ package com.guadalavila.todofirstversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.guadalavila.todofirstversion.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        var listTasks = mutableListOf<String>();
+
+        binding.totalTasksText.text = "Total de Tareas: ${listTasks.size}";
+
+        binding.btnAddTask.setOnClickListener {
+            listTasks.add(binding.taskEdit.toString());
+            binding.totalTasksText.text = "Total de Tareas: ${listTasks.size}";
+            binding.taskEdit.setText("")
+            Log.d("Main", "Se agrego!")
+        }
     }
 }
