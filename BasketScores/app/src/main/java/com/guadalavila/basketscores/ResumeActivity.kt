cb126b252d14d82eb66cad0276596b1ea3230e81
guadalupe <guadalupe.alavila@gmail.com>
2023-02-11 17:37:27 -0300

    fix style bascket app

--- BasketScores/app/src/main/java/com/guadalavila/basketscores/ResumeActivity.kt
+++ BasketScores/app/src/main/java/com/guadalavila/basketscores/ResumeActivity.kt
@@ -3,6 +3,7 @@ package com.guadalavila.basketscores
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.guadalavila.basketscores.databinding.ActivityResumeBinding
 
 class ResumeActivity : AppCompatActivity() {
@@ -20,6 +21,8 @@ class ResumeActivity : AppCompatActivity() {
         val extras = intent.extras;
         val visitScores: Int = extras?.getInt(VISIT_KEY) ?: 0 ;
         val localScores: Int = extras?.getInt(LOCAL_KEY) ?: 0 ;
+
+        Log.d("el rdo", visitScores.toString())
 
         binding.resultEdit.text = "${localScores.toString()} - ${visitScores.toString()}"
 
